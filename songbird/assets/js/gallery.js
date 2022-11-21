e6fa3023fb5b31e84af1949aa00995c9c421f0b5
Farkhod Kodirov <farxod94@gmail.com>
2022-11-22 04:46:55 +0500

    fix: audio-player gallery.html

--- songbird/assets/js/gallery.js
+++ songbird/assets/js/gallery.js
@@ -21,13 +21,15 @@ function sectotime(sec){
 
 const allaudio = document.querySelectorAll('.audio-player');
 allaudio.forEach((item,index) => {
-  item.addEventListener('click', function playpause(e){console.log(e.target);
-    document.querySelectorAll('.audio-player')[index].children[0].src=birds[Math.trunc(index/6)][index%6]['audio'];
+  item.addEventListener('click', function playpause(e){
+    if(!document.querySelectorAll('.audio-player')[index].children[0].src.includes('.mp3')){
+    document.querySelectorAll('.audio-player')[index].children[0].src=birds[Math.trunc(index/6)][index%6]['audio'];}
     let audio=e.target.parentElement.parentElement.parentElement.children[0];
     audio.onloadedmetadata= function(){
       e.target.parentNode.nextElementSibling.children[1].children[1].innerHTML=sectotime(audio.duration);
       document.querySelectorAll('.timebar')[index].step=Math.floor(100/Math.floor(audio.duration)*100)/100;
     }
+      if(e.target.tagName=='I'){
     if(audio.paused==true){
       audio.play();
       e.target.classList.remove('fa-circle-play');
@@ -38,5 +40,50 @@ allaudio.forEach((item,index) => {
       e.target.classList.remove('fa-circle-pause');
       e.target.classList.add('fa-circle-play');
     }
+    }
+    audio.addEventListener("timeupdate", function(){
+      if(!document.querySelectorAll('.timebar')[index].classList.contains('changing')){
+        document.querySelectorAll('.timebar')[index].value=Math.floor(audio.currentTime)/Math.floor(audio.duration)*100;}
+        document.querySelectorAll('.current-time')[index].innerHTML=sectotime(audio.currentTime);
+      if(audio.currentTime==audio.duration){
+        document.querySelectorAll('.playback-button')[index].childNodes[0].classList.remove('fa-circle-pause');
+        document.querySelectorAll('.playback-button')[index].childNodes[0].classList.add('fa-circle-play');
+        document.querySelectorAll('.current-time')[index].innerHTML='00:00';
+      }
+    }, false);
+
+
+
+    document.querySelectorAll('.timebar')[index].addEventListener("change", function(){
+      audio.currentTime=document.querySelectorAll('.timebar')[index].value*audio.duration/100;
+      //console.log(bar.value*audio.duration/100);
+        
+      }, false);
+      
+      
+      
+      document.querySelectorAll('.timebar')[index].addEventListener("mousedown", function(){
+        document.querySelectorAll('.timebar')[index].classList.add("changing");  
+        }, false);
+      
+      
+      
+        document.querySelectorAll('.timebar')[index].addEventListener("mouseup", function(){
+          document.querySelectorAll('.timebar')[index].classList.remove("changing"); 
+        }, false);
+  
+  
+  
   });
+
+
+
+
+
+
+
+
+
+
+
 });
